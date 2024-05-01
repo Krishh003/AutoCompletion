fc68fb57dacc1623f49b17445312f6ccd17fe070
Krishh003 <akarshhankumar003@gmail.com>
2024-05-02 01:14:39 +0530

    Added Menu By Saumya (fr this time)

--- main.cpp
+++ main.cpp
@@ -9,25 +9,135 @@ int main()
 {
 	SetConsoleOutputCP(CP_UTF8);
 	// creating the root of the trie
-	TrieNode *root = createTrieNode('#');
-	unordered_map<string, string> hmap;
-	bool success = setup_trie(root);
-	bool check = setup_emoji(hmap);
-	if (success && check)
-	{
+    TrieNode *root = createTrieNode('#');
+    int choice;
+    std::string word;
+    unordered_map<string, string> hmap;
+    bool success = setup_trie(root);
+    bool check = setup_emoji(hmap);
 
-		string query;
-		cout << "Enter query : ";
-		cin >> query;
-		cout << endl;
+    if (success && check) {
+        while (true) {
+    std::cout << "Main Menu:" << std::endl;
+    std::cout << "1. Auto Complete" << std::endl;
+    std::cout << "2. Additional Functionalities" << std::endl;
+    std::cout << "3. Exit" << std::endl;
+    std::cout << "Enter your choice: ";
+    std::cin >> choice;
 
-		
-		printAutoComplete(root, query, hmap);
-	}
-	else
-	{
-		cout << "internal error! cannot proceed\nexiting program...";
-	}
+    switch (choice) {
+        case 1:
+            std::cout << "Enter the prefix for autocomplete: ";
+            std::cin >> word;
+            {
+                bool isValidInput = true;
+                for (char c : word) {
+                    if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))) {
+                        isValidInput = false;
+                        break;
+                    }
+                }
+                if (isValidInput) {
+                     printAutoComplete(root, word,hmap);
+                } else {
+                    std::cout << "Invalid input! Please enter a word containing only alphabets." << std::endl;
+                }
+            }
+            break;
+        case 2:
+            while(true) {
+                std::cout << "Additional Functionalities:" << std::endl;
+                std::cout << "1. Insert a word" << std::endl;
+                std::cout << "2. Delete a word" << std::endl;
+                std::cout << "3. Search for a word" << std::endl;
+                std::cout << "4. Display all words" << std::endl;
+                std::cout << "5. Return to Main Menu" << std::endl;
+                std::cout << "6. Exit Program" << std::endl;
+                std::cout << "Enter your choice: ";
+                std::cin >> choice;
+
+                switch (choice) {
+                    case 1:
+                        std::cout << "Enter the word to insert: ";
+                        std::cin >> word;
+                        {
+                            bool isValidInput = true;
+                            for (char c : word) {
+                                if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))) {
+                                    isValidInput = false;
+                                    break;
+                                }
+                            }
+                            if (isValidInput) {
+                                insertTrieNode(root, word);
+                            } else {
+                                std::cout << "Invalid input! Please enter a word containing only alphabets." << std::endl;
+                            }
+                        }
+                        break;
+                    case 2:
+                        std::cout << "Enter the word to delete: ";
+                        std::cin >> word;
+                        {
+                            bool isValidInput = true;
+                            for (char c : word) {
+                                if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))) {
+                                    isValidInput = false;
+                                    break;
+                                }
+                            }
+                            if (isValidInput) {
+                                deleteTrieNode(root, word);
+                            } else {
+                                std::cout << "Invalid input! Please enter a word containing only alphabets." << std::endl;
+                            }
+                        }
+                        break;
+                    case 3:
+                        std::cout << "Enter the word to search: ";
+                        std::cin >> word;
+                        {
+                            bool isValidInput = true;
+                            for (char c : word) {
+                                if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))) {
+                                    isValidInput = false;
+                                    break;
+                                }
+                            }
+                            if (isValidInput) {
+                                searchTrieNode(root, word);
+                            } else {
+                                std::cout << "Invalid input! Please enter a word containing only alphabets." << std::endl;
+                            }
+                        }
+                        break;
+                    case 4:
+                        std::cout << "All words in the trie:" << std::endl;
+                        displayTrie(root, "");
+                        break;
+                    case 5:
+                        break; // Return to Main Menu
+                    case 6:
+                        std::cout << "Exiting program..." << std::endl;
+                        return 0;
+                    default:
+                        std::cout << "Invalid choice. Please try again." << std::endl;
+                }
+                if (choice == 5) break;
+            }
+            break;
+        case 3:
+            std::cout << "Exiting program..." << std::endl;
+            return 0;
+        default:
+            std::cout << "Invalid choice. Please try again." << std::endl;
+    }
+}
+
+
+    } else {
+        cout << "internal error! cannot proceed\nexiting program...";
+    }
 
 	return 0;
 }
